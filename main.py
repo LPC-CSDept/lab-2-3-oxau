9fbcf99690bebb87ba11a44025f28321412c2f01
oxau <notoriousact@gmail.com>
2023-02-05 20:33:26 -0800

    ocococo

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    val1 = int(input('First Integer: '))
+    val2 = int(input('Second Integer: '))
+    val3 = int(input('Third Integer: '))
+    sum = int(val1 + val2 + val3)
+    avg = float(sum/3)
 
+    print ('Values: ' + '{0} {1} {2}'. format(val1, val2, val3))
+    print (f'Total: \t\t{sum}')
+    print (f'Average: \t{avg:.2f}')
+
+    
     pass
 
 
